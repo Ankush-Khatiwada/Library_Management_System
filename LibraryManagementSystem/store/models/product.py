23af48d36ca7d7142f88c1b8f6a9dc875b9c6e37
Ankush-Khatiwada <ankush.khatiwada@gmail.com>
2021-05-07 21:28:13 +0545

    debugged error in footer

--- LibraryManagementSystem/store/models/product.py
+++ LibraryManagementSystem/store/models/product.py
@@ -6,8 +6,8 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     Isbn = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=200, default='' , null=True , blank=True)
-    author= models.CharField(max_length=200,default='',null=True,blank=True)
+    description = models.CharField(max_length=200, default='' , null=False , blank=True)
+    author= models.CharField(max_length=200,default='',null=False,blank=True)
     image = models.ImageField(upload_to='uploads/products/')
 
     @staticmethod
