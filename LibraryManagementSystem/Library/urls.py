aa0c7c95cd661fe6a1eb097a4dbafac586195277
Ankush-Khatiwada <ankush.khatiwada@gmail.com>
2021-05-10 20:49:19 +0545

    admin css debugged

--- LibraryManagementSystem/Library/urls.py
+++ LibraryManagementSystem/Library/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('store.urls')),
 
-
+ url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     #Heroku to render data
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
