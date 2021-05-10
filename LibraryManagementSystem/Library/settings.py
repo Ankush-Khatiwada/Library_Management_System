61566e5bbc2e24d65e2ae11d3f6a16db1e1174c1
Ankush-Khatiwada <ankush.khatiwada@gmail.com>
2021-05-10 19:36:24 +0545

    changed allowed host

--- LibraryManagementSystem/Library/settings.py
+++ LibraryManagementSystem/Library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =['127.0.0.1']
+ALLOWED_HOSTS =['127.0.0.1','helibrarycollaborativedevelopme.herokuapp.com']
 
 
 # Application definition
